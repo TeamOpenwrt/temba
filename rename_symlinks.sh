3089756ce7e6b5922da1d3ccee7f6d0a01d7e3b3
pedrolab <pedrolab@example.com>
2018-07-06 16:20:04 +0200

    change symlinks, provide a helper
    - common directory now is in template__common
    - provided a script to do (future) massive relative symlink renaming
    - explain this relative symlinking in README

---
+++ rename_symlinks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script is a helper when you want to massively rename relative symlinks. It creates another script that you verify before executing it.
+# It contains example when I decided to change `common` directory to `template__common` directory.
+# Warning, if you apply again, this mess is going to happen
+#
+#    -../../../template__common/regular/bmx6.erb
+#    +../../../template__template__common/regular/bmx6.erb
+
+# thanks! https://superuser.com/a/157832
+
+SRC_DIR="." # current directory
+OLD_TARGET="*common*" # match symbolic link affected
+SUB="s/common/template__common/" # what is the substitution you want to apply
+
+# why ln-nsf -> src https://superuser.com/questions/81164/why-create-a-link-like-this-ln-nsf
+find $SRC_DIR -type l \
+  -lname "$OLD_TARGET/*" -printf \
+  'ln -nsf "$(readlink "%p"|sed '$SUB')" "$(echo "%p"|sed '$SUB')"\n' \
+  > do_rename_symlinks.sh
+
+chmod +x do_rename_symlinks.sh
