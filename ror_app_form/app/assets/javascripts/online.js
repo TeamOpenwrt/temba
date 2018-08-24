0bbe3723dc235136212dfd46dbb8d3a636de1a3a
pedrolab <pedrolab@example.com>
2018-08-24 15:07:12 +0200

    feature: online users
    as the resource is scarce is useful for the user to know how much people is currently planning to use it

---
+++ ror_app_form/app/assets/javascripts/online.js
@@ -0,0 +1,32 @@
+(function() {
+  var actioncable_methods
+
+  actioncable_methods = {
+    connected: function() {
+      console.log('connected to temba form')
+      App.online.update_users_counter()
+    },
+    disconnected: function() {
+      App.cable.subscriptions.remove(this)
+      this.perform('unsubscribed')
+    },
+    received: function(data) {
+      var val
+      // Called when there's incoming data on the websocket for this channel
+      val = data.counter - 1 // just count other users
+      //update "users_counter"-element in view:
+      document.getElementById('users').textContent = val
+
+      console.log('new event')
+      return App.vue.receiveMessage(data)
+    },
+    update_users_counter() {
+      this.perform('update_users_counter')
+    }
+  }
+
+  App.online = App.cable.subscriptions.create({
+    channel: "OnlineChannel"
+  }, actioncable_methods)
+
+}).call(this)
