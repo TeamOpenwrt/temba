3137ca4a633595734a750b3b3381c6d1e4c30fd3
pedrolab <pedrolab@example.com>
2018-12-13 18:09:32 +0100

    add dropbear/authorized_keys
    it also formalize the way of operating with new symlinks through a utils directory

---
+++ utils/new_symlinks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script is a helper when you want to massively add a new symlink to all
+# targets. It creates another script that you verify before executing it. It
+# contains example when I decided to change `common` directory to
+# `template__common` directory.
+# Warning, if you apply again, this mess is going to happen
+#
+#    -../../../template__common/regular/bmx6.erb
+#    +../../../template__template__common/regular/bmx6.erb
+
+# thanks! https://superuser.com/a/157832
+
+DIR="../files/" # template directory
+TARGETS="template__*" # match symbolic link affected
+SRC="library/dropbear-authorized_keys.erb"
+DST_PATH="/etc/dropbear"
+DST_FILE="${DST_PATH}/authorized_keys.erb" # what is the substitution you want to apply
+
+# why ln-nsf -> src https://superuser.com/questions/81164/why-create-a-link-like-this-ln-nsf
+find $DIR -iname "$TARGETS" -printf \
+  'mkdir -p "%p'"$DST_PATH"'"; ln -nsf "../../../'"$SRC"'" "%p'"$DST_FILE"'"\n' \
+  > do_new_symlinks.sh
+
+chmod +x do_new_symlinks.sh
+
+echo results:
+cat do_new_symlinks.sh
