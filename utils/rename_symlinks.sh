56af1deca9cb89c6e7f79dba86bf7d1449c52c7f
pedrolab <pedrolab@example.com>
2018-12-13 18:17:07 +0100

    fix scripts in utils dir

--- utils/rename_symlinks.sh
+++ utils/rename_symlinks.sh
@@ -2,16 +2,16 @@
 
 # This script is a helper when you want to massively rename relative symlinks.
 # It creates another script that you verify before executing it. It contains
-# example when I decided to change `common` directory to `template__common`
-# directory.
-# Warning, if you apply again, this mess is going to happen
+# example when I decided to change all symlinks that pointed to
+# `template_common` to rename it to `library`
+# Warning, if you apply again, symlinks are going to change to
 #
-#    -../../../template__common/regular/bmx6.erb
-#    +../../../template__template__common/regular/bmx6.erb
+#    -../../../template__common
+#    +../../../library
 
 # thanks! https://superuser.com/a/157832
 
-SRC_DIR="." # current directory
+SRC_DIR=".." # current directory
 OLD_TARGET="*common*" # match symbolic link affected
 SUB="s/template__common/library/" # what is the substitution you want to apply
 
