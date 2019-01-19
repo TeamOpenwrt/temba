ddba7370858929d9db4e4b4702e8d4051049c520
pedrolab <531310-pedrolab@users.noreply.gitlab.com>
2019-01-19 22:55:52 +0100

    move and improve limesdk-files

---
+++ limesdk-files/prepare_limesdk.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+## work in progress (only intended to work on 18.06.1)
+
+set -e   # causes the shell to exit if any subcommand or pipeline returns a non-zero status.
+set -v
+
+# thanks https://unix.stackexchange.com/a/52801
+if [[ $(dirname $0) != . ]]; then
+  echo 'ERROR: execute script in the same directory (no absolute nor relative path)'
+  exit 1
+fi
+
+# src https://gitlab.com/guifi-exo/wiki/blob/master/howto/lime-sdk.md
+git clone https://github.com/libremesh/lime-sdk ../lime-sdk
+
+cd ../../lime-sdk
+
+# override release
+cp ../temba/limesdk-files/options.conf.local .
+
+cp ../temba/limesdk-files/feeds.conf.temba .
+
+# cooker does not work from other link
+./cooker -f
+
+# develop environment patch
+cp ../temba/limesdk-files/regdbtz18.sh snippets/
+snippets/regdbtz18.sh
+
+# get specific version of bmx6 -> src https://gitlab.com/guifi-exo/wiki/blob/master/howto/openwrt_template.md
+# more https://github.com/bmx-routing/bmx6/tree/2a87b770d3f9c254e3927dc159e2f425f2e0e83a
+# warning: this assumes temba git repo is named temba and it is in the same level as lime-sdk
+cp ../temba/bmx6_Makefile feeds/routing/bmx6/Makefile
+
+# ar71xx hardcoded
+./cooker -b ar71xx/generic --force-local
---
+++ limesdk-files/regdbtz18.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# develop environment patch for openwrt18
+# src https://github.com/libremesh/lime-sdk/blob/develop/snippets/regdbtz.sh
+# extra ref https://dev.qmp.cat/projects/qmp-cooker/repository/revisions/master/entry/snippets/regdbus.sh
+
+. options.conf
+
+base_feed="$feeds_dir/base"
+echo "Patch will be applied to $base_feed"
+
+[ ! -d $base_feed ] && {
+  echo "$base_feed does not exist. Before applying this snippet you must clone the feeds (option -f of cooker)"
+  exit 1
+}
+
+patch_file="$PWD/$tmp_dir/999-tz_hack.patch"
+
+cat > $patch_file << EOF
+--- a/db.txt
++++ b/db.txt
+@@ -1209,8 +1209,8 @@ country TW: DFS-FCC
+    (57000 - 66000 @ 2160), (40)
+
+ country TZ:
+-   (2402 - 2482 @ 40), (20)
+-   (5735 - 5835 @ 80), (30)
++   (2402 - 2484 @ 40), (30)
++   (5150 - 5835 @ 80), (30)
+
+ # Source:
+ # #914 / 06 Sep 2007: http://www.ucrf.gov.ua/uk/doc/nkrz/1196068874
+EOF
+
+( cd $base_feed && mv $patch_file package/firmware/wireless-regdb/patches/ && {
+  echo "Patch copied, now you can use the special channel TZ when you deploy a mesh network on International Waters"
+} || echo "Patch copy error, maybe it is already applied or OpenWRT source has changed" )
